409253effb4bce79b51780143382102a4c3fb31b
rheno felipe <92105197+rheno911@users.noreply.github.com>
2026-02-28 23:35:46 -0600

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
-    kotlin("plugin.serialization")
+    kotlin("plugin.serialization") version "1.9.10"
 }
 
 android {
@@ -12,19 +12,27 @@ android {
         minSdk = 21
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
 
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
-    
     kotlinOptions {
         jvmTarget = "1.8"
     }
 }
 
 dependencies {
-    implementation("com.github.recloudstream:core:3.+")
+    implementation("com.github.recloudstream:cloudstream3-core:3.3.+")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
 }
